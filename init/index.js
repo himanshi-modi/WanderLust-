3991db054ea6f37ca7583ea8d2a81af5df0a4bf3
Himanshi Modi <himanshimodi421@gmail.com>
2026-07-15 01:22:20 +0530

    Fix MongoDB connection

--- init/index.js
+++ init/index.js
@@ -1,4 +1,7 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 require("dotenv").config({ path: "../.env" });
+
 const mongoose =require("mongoose");
 const initdata=require("./data.js")
 const Listing=require("../models/listing.js");
@@ -11,21 +14,24 @@ const geocodingClient = mbxGeocoding({
 });
 
 async function main(){
-   await mongoose.connect('mongodb://MERNUSER:Mern12345@ac-afsbq6p-shard-00-00.afxjaa6.mongodb.net:27017,ac-afsbq6p-shard-00-01.afxjaa6.mongodb.net:27017,ac-afsbq6p-shard-00-02.afxjaa6.mongodb.net:27017/test?ssl=true&replicaSet=atlas-2xezdt-shard-0&authSource=admin&appName=Cluster0');
+   await mongoose.connect(process.env.CLOUD_DATABASE_URL);
 }
 
 main()
-    .then(async () => {
-        console.log("Connected to DB!");
-        await initDB();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
+  .then(async () => {
+    console.log("Connected to DB!");
+    try {
+      await initDB();
+    } catch (err) {
+      console.error("INIT DB ERROR:", err);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 const initDB = async () => {
     await Listing.deleteMany({});
-
+    
     let listings = initdata.data.map((obj) => ({
         ...obj,
         owner: "6a2fbdf1d786ce1e3008396d",
@@ -35,14 +41,15 @@ const initDB = async () => {
 
     
     for (let listing of listings) {
-       
+        console.log("Uploading:", listing.title);
+
         const result = await cloudinary.uploader.upload(
             listing.image.url,
             {
                 folder: "wanderlust_DEV",
             }
         );
-        
+        console.log("Uploaded:", listing.title);
         listing.image = {
             url: result.secure_url,
             filename: result.public_id,
@@ -53,7 +60,7 @@ const initDB = async () => {
                 limit: 1,
             })
             .send();
-
+            console.log("Geocoded:", listing.title);
             listing.geometry = geoData.body.features[0].geometry;
     }
 
